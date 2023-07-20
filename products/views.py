c2f66d5d933e1d1097dfcde91e452fd2b6cef729
Nic <nicolecadman@icloud.com>
2023-07-20 20:37:12 +0000

    added sorted counts and product counts to loading page

--- products/views.py
+++ products/views.py
@@ -18,16 +18,17 @@ def all_products(request):
         if 'sort' in request.GET:
             sortkey = request.GET['sort']
             sort = sortkey
-            if 'sort' == 'name':
+            if sortkey == 'name':
                 sortkey = 'lower_name'
                 products = products.annotate(lower_name=Lower('name'))
-
+            if sortkey == 'category':
+                sortkey = 'category__name'
             if 'direction' in request.GET:
                 direction = request.GET['direction']
-                if 'direction' == 'desc':
+                if direction == 'desc':
                     sortkey = f'-{sortkey}'
             products = products.order_by(sortkey)
-
+            
         if 'category' in request.GET:
             categories = request.GET['category'].split(',')
             products = products.filter(category__name__in=categories)
