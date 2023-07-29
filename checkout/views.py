77bf8f7ec82e8e2356839dca7e09a643a79487fe
Nic <nicolecadman@icloud.com>
2023-07-29 15:00:38 +0000

    added stripe elements

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51NZEM9KWzixhQyQGPGfKmHgsDzSMjRg2VoL9xVH0cVXEXa34ObHB3Kmw6D5XtpvR3U59jjBl9kLWNWpTvKXfNBs200g9gPJi3I',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
