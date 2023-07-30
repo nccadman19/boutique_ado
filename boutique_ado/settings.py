3fa086017c039b795ac09671c0e7343066742bcd
Nic <nicolecadman@icloud.com>
2023-07-30 15:37:02 +0000

    updated checkout form

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -25,6 +25,8 @@ SECRET_KEY = 'p@ei#69*b*zz3u4yie-$()@cy^l(+x9&@6ypx+r0lm(3%_9hr7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
+DEFAULT_AUTO_FIELD = 'django.db.models.AutoField'
+
 ALLOWED_HOSTS = ['8000-nccadman19-boutiqueadov-jqg4qsore1x.ws-eu102.gitpod.io']
 
 
